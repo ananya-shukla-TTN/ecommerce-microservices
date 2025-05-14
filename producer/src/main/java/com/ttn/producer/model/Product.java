cd378bab71858bdf2b24d0e0f53db882add91975
Ananya Shukla <ananya.shukla@tothenew.com>
2025-05-14 14:24:03 +0530

    Add product entity and repository

---
+++ producer/src/main/java/com/ttn/producer/model/Product.java
@@ -0,0 +1,25 @@
+package com.ttn.producer.model;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document(collection = "products")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class Product {
+    @Id
+    private String id;
+
+    private String name;
+    private BigDecimal price;
+    private Integer availableQuantity;
+    private String category;
+}
